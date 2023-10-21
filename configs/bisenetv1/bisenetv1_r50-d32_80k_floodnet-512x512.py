7af4c550d088f8a6f78f4413b0ed38736dc02dc2
manhhv87 <manhhv87@gmail.com>
2023-10-21 23:28:43 +0700

    modify code

--- configs/bisenetv1/bisenetv1_r50-d32_80k_floodnet-512x512.py
+++ configs/bisenetv1/bisenetv1_r50-d32_80k_floodnet-512x512.py
@@ -7,6 +7,8 @@ _base_ = [
 
 crop_size = (512, 512)
 data_preprocessor = dict(size=crop_size)
+checkpoint='https://download.openmmlab.com/mmsegmentation/v0.5/bisenetv1/bisenetv1_r50-d32_lr5e-3_4x4_512x512_160k_coco-stuff164k/bisenetv1_r50-d32_lr5e-3_4x4_512x512_160k_coco-stuff164k_20211101_040616-d2bb0df4.pth'
+
 norm_cfg = dict(type='SyncBN', requires_grad=True)
 
 model = dict(
@@ -16,7 +18,10 @@ model = dict(
         context_channels=(512, 1024, 2048),
         spatial_channels=(256, 256, 256, 512),
         out_channels=1024,
-        backbone_cfg=dict(type='ResNet', depth=50)),
+        backbone_cfg=dict(
+          type='ResNet', depth=50,
+          init_cfg=dict(type='Pretrained', checkpoint=checkpoint))
+          ),        
 
     decode_head=dict(in_channels=1024, channels=1024, num_classes=10),
 
