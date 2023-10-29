b234c06eefea913ec6393521288460464b28059e
manhhv87 <manhhv87@gmail.com>
2023-10-29 22:45:26 +0700

    add unetformer

---
+++ configs/_base_/models/my_unetformer.py
@@ -0,0 +1,43 @@
+# model settings
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+
+data_preprocessor = dict(
+    type='SegDataPreProcessor',
+    mean=[0.485, 0.456, 0.406],
+    std=[0.229, 0.224, 0.225],
+    bgr_to_rgb=True,
+    pad_val=0,
+    seg_pad_val=255)
+
+model = dict(
+    type='EncoderDecoder',
+    data_preprocessor=data_preprocessor,
+    pretrained='open-mmlab://resnet50_v1c',
+
+    backbone=dict(
+        type='ResNetV1c',
+        depth=50,
+        num_stages=4,
+        out_indices=(1, 2, 3, 4),
+        dilations=(1, 1, 1, 1),
+        strides=(1, 2, 2, 2),
+        norm_cfg=norm_cfg,
+        norm_eval=False,
+        style='pytorch',
+        contract_dilation=True),
+
+    decode_head=dict(
+        type='UnetformerHead',
+        in_channels=[64, 128, 256, 512],
+        in_index=[1, 2, 3, 4],
+        channels=64,
+        dropout_ratio=0.1,
+        num_classes=10,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
