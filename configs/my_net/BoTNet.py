9519b9a595283e48ae747ee5136aead22a84246f
manhhv87 <manhhv87@gmail.com>
2023-11-20 17:47:51 +0700

    add BoTNet

--- configs/my_net/BoTNet.py
+++ configs/my_net/BoTNet.py
@@ -1,243 +1,48 @@
-import math
-import torch
-from torch import nn, einsum
+_base_ = [
+    '../_base_/models/bisenetv2.py',
+    '../_base_/datasets/floodnet.py',
+    '../_base_/default_runtime.py', 
+    '../_base_/schedules/schedule_80k.py'
+]
 
-from einops import rearrange
+crop_size = (512, 512)
+data_preprocessor = dict(size=crop_size)
+checkpoint='https://download.openmmlab.com/mmsegmentation/v0.5/bisenetv2/bisenetv2_fcn_4x8_1024x1024_160k_cityscapes/bisenetv2_fcn_4x8_1024x1024_160k_cityscapes_20210903_000032-e1a2eed6.pth'
 
-# translated from tensorflow code
-# https://gist.github.com/aravindsrinivas/56359b79f0ce4449bcb04ab4b56a57a2
+model = dict(
+    data_preprocessor=data_preprocessor,
+    pretrained=checkpoint,
+    decode_head=dict(
+        num_classes=10,
+        loss_decode=[
+            dict(type='CrossEntropyLoss', loss_name='loss_ce', use_sigmoid=False, loss_weight=0.3),
+            dict(type='DiceLoss', loss_name='loss_dice', loss_weight=0.7)]))
 
-# positional embedding helpers
+optim_wrapper = dict(
+    _delete_=True,
+    type='OptimWrapper',
+    optimizer=dict(
+        type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01),
+    paramwise_cfg=dict(
+        custom_keys={
+            'pos_block': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.),
+            'head': dict(lr_mult=10.)
+        }))
 
-def pair(x):
-    return (x, x) if not isinstance(x, tuple) else x
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=1e-6, by_epoch=False, begin=0, end=8000),
+    dict(
+        type='PolyLR',
+        eta_min=0.0,
+        power=1.0,
+        begin=8000,
+        end=80000,
+        by_epoch=False,
+    )
+]
 
-def expand_dim(t, dim, k):
-    t = t.unsqueeze(dim = dim)
-    expand_shape = [-1] * len(t.shape)
-    expand_shape[dim] = k
-    return t.expand(*expand_shape)
-
-def rel_to_abs(x):
-    b, h, l, _, device, dtype = *x.shape, x.device, x.dtype
-    dd = {'device': device, 'dtype': dtype}
-    col_pad = torch.zeros((b, h, l, 1), **dd)
-    x = torch.cat((x, col_pad), dim = 3)
-    flat_x = rearrange(x, 'b h l c -> b h (l c)')
-    flat_pad = torch.zeros((b, h, l - 1), **dd)
-    flat_x_padded = torch.cat((flat_x, flat_pad), dim = 2)
-    final_x = flat_x_padded.reshape(b, h, l + 1, 2 * l - 1)
-    final_x = final_x[:, :, :l, (l-1):]
-    return final_x
-
-def relative_logits_1d(q, rel_k):
-    b, heads, h, w, dim = q.shape
-    logits = einsum('b h x y d, r d -> b h x y r', q, rel_k)
-    logits = rearrange(logits, 'b h x y r -> b (h x) y r')
-    logits = rel_to_abs(logits)
-    logits = logits.reshape(b, heads, h, w, w)
-    logits = expand_dim(logits, dim = 3, k = h)
-    return logits
-
-# positional embeddings
-
-class AbsPosEmb(nn.Module):
-    def __init__(
-        self,
-        fmap_size,
-        dim_head
-    ):
-        super().__init__()
-        height, width = pair(fmap_size)
-        scale = dim_head ** -0.5
-        self.height = nn.Parameter(torch.randn(height, dim_head) * scale)
-        self.width = nn.Parameter(torch.randn(width, dim_head) * scale)
-
-    def forward(self, q):
-        emb = rearrange(self.height, 'h d -> h () d') + rearrange(self.width, 'w d -> () w d')
-        emb = rearrange(emb, ' h w d -> (h w) d')
-        logits = einsum('b h i d, j d -> b h i j', q, emb)
-        return logits
-
-class RelPosEmb(nn.Module):
-    def __init__(
-        self,
-        fmap_size,
-        dim_head
-    ):
-        super().__init__()
-        height, width = pair(fmap_size)
-        scale = dim_head ** -0.5
-        self.fmap_size = fmap_size
-        self.rel_height = nn.Parameter(torch.randn(height * 2 - 1, dim_head) * scale)
-        self.rel_width = nn.Parameter(torch.randn(width * 2 - 1, dim_head) * scale)
-
-    def forward(self, q):
-        h, w = self.fmap_size
-
-        q = rearrange(q, 'b h (x y) d -> b h x y d', x = h, y = w)
-        rel_logits_w = relative_logits_1d(q, self.rel_width)
-        rel_logits_w = rearrange(rel_logits_w, 'b h x i y j-> b h (x y) (i j)')
-
-        q = rearrange(q, 'b h x y d -> b h y x d')
-        rel_logits_h = relative_logits_1d(q, self.rel_height)
-        rel_logits_h = rearrange(rel_logits_h, 'b h x i y j -> b h (y x) (j i)')
-        return rel_logits_w + rel_logits_h
-
-# classes
-
-class Attention(nn.Module):
-    def __init__(
-        self,
-        *,
-        dim,
-        fmap_size,
-        heads = 4,
-        dim_head = 128,
-        rel_pos_emb = False
-    ):
-        super().__init__()
-        self.heads = heads
-        self.scale = dim_head ** -0.5
-        inner_dim = heads * dim_head
-
-        self.to_qkv = nn.Conv2d(dim, inner_dim * 3, 1, bias = False)
-
-        rel_pos_class = AbsPosEmb if not rel_pos_emb else RelPosEmb
-        self.pos_emb = rel_pos_class(fmap_size, dim_head)
-
-    def forward(self, fmap):
-        heads, b, c, h, w = self.heads, *fmap.shape
-
-        q, k, v = self.to_qkv(fmap).chunk(3, dim = 1)
-        q, k, v = map(lambda t: rearrange(t, 'b (h d) x y -> b h (x y) d', h = heads), (q, k, v))
-
-        q = q * self.scale
-
-        sim = einsum('b h i d, b h j d -> b h i j', q, k)
-        sim = sim + self.pos_emb(q)
-
-        attn = sim.softmax(dim = -1)
-
-        out = einsum('b h i j, b h j d -> b h i d', attn, v)
-        out = rearrange(out, 'b h (x y) d -> b (h d) x y', x = h, y = w)
-        return out
-
-class BottleBlock(nn.Module):
-    def __init__(
-        self,
-        *,
-        dim,
-        fmap_size,
-        dim_out,
-        proj_factor,
-        downsample,
-        heads = 4,
-        dim_head = 128,
-        rel_pos_emb = False,
-        activation = nn.ReLU()
-    ):
-        super().__init__()
-
-        # shortcut
-
-        if dim != dim_out or downsample:
-            kernel_size, stride, padding = (3, 2, 1) if downsample else (1, 1, 0)
-
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(dim, dim_out, kernel_size, stride = stride, padding = padding, bias = False),
-                nn.BatchNorm2d(dim_out),
-                activation
-            )
-        else:
-            self.shortcut = nn.Identity()
-
-        # contraction and expansion
-
-        attn_dim_in = dim_out // proj_factor
-        attn_dim_out = heads * dim_head
-
-        self.net = nn.Sequential(
-            nn.Conv2d(dim, attn_dim_in, 1, bias = False),
-            nn.BatchNorm2d(attn_dim_in),
-            activation,
-            Attention(
-                dim = attn_dim_in,
-                fmap_size = fmap_size,
-                heads = heads,
-                dim_head = dim_head,
-                rel_pos_emb = rel_pos_emb
-            ),
-            nn.AvgPool2d((2, 2)) if downsample else nn.Identity(),
-            nn.BatchNorm2d(attn_dim_out),
-            activation,
-            nn.Conv2d(attn_dim_out, dim_out, 1, bias = False),
-            nn.BatchNorm2d(dim_out)
-        )
-
-        # init last batch norm gamma to zero
-
-        nn.init.zeros_(self.net[-1].weight)
-
-        # final activation
-
-        self.activation = activation
-
-    def forward(self, x):
-        shortcut = self.shortcut(x)
-        x = self.net(x)
-        x = x + shortcut
-        return self.activation(x)
-
-# main bottle stack
-
-class BottleStack(nn.Module):
-    def __init__(
-        self,
-        *,
-        dim,
-        fmap_size,
-        dim_out = 2048,
-        proj_factor = 4,
-        num_layers = 3,
-        heads = 4,
-        dim_head = 128,
-        downsample = True,
-        rel_pos_emb = False,
-        activation = nn.ReLU()
-    ):
-        super().__init__()
-        fmap_size = pair(fmap_size)
-
-        self.dim = dim
-        self.fmap_size = fmap_size
-
-        layers = []
-
-        for i in range(num_layers):
-            is_first = i == 0
-            dim = (dim if is_first else dim_out)
-            layer_downsample = is_first and downsample
-
-            fmap_divisor = (2 if downsample and not is_first else 1)
-            layer_fmap_size = tuple(map(lambda t: t // fmap_divisor, fmap_size))
-
-            layers.append(BottleBlock(
-                dim = dim,
-                fmap_size = layer_fmap_size,
-                dim_out = dim_out,
-                proj_factor = proj_factor,
-                heads = heads,
-                dim_head = dim_head,
-                downsample = layer_downsample,
-                rel_pos_emb = rel_pos_emb,
-                activation = activation
-            ))
-
-        self.net = nn.Sequential(*layers)
-
-    def forward(self, x):
-        _, c, h, w = x.shape
-        assert c == self.dim, f'channels of feature map {c} must match channels given at init {self.dim}'
-        assert h == self.fmap_size[0] and w == self.fmap_size[1], f'height and width ({h} {w}) of feature map must match the fmap_size given at init {self.fmap_size}'
-        return self.net(x)
+train_dataloader = dict(batch_size=2, num_workers=2)
+val_dataloader = dict(batch_size=1, num_workers=4)
+test_dataloader = val_dataloader
