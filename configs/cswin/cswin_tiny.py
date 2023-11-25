e26a4be31aff14f2a1f3d527eb97e489396f4d30
manhhv87 <manhhv87@gmail.com>
2023-11-25 22:43:57 +0700

    modify code

--- configs/cswin/cswin_tiny.py
+++ configs/cswin/cswin_tiny.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/dcswin.py',
+    '../_base_/models/cswin.py',
     '../_base_/datasets/floodnet.py',
     '../_base_/default_runtime.py',
     '../_base_/schedules/schedule_80k.py'
@@ -7,25 +7,24 @@ _base_ = [
 
 crop_size = (512, 512)
 data_preprocessor = dict(size=crop_size)
-checkpoint='https://drive.usercontent.google.com/download?id=1tHNxQUffwNIfWFDKa4ql1klKGjCnbhwv&export=download&authuser=0&confirm=t&uuid=3f0fc36c-9936-4d25-9187-33b43954afe5&at=APZUnTXkSlUg7GRvllIndBNc0acn:1700898173032'
+checkpoint='https://objects.githubusercontent.com/github-production-release-asset-2e65be/382210636/c1c00b47-20af-4b20-9d23-ae74f0f61aab?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20231125%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231125T134439Z&X-Amz-Expires=300&X-Amz-Signature=534eb1cb946c4f8d0bf75b8371122921b93f8eb18a5d4bb0090cd45ec1b70263&X-Amz-SignedHeaders=host&actor_id=67886698&key_id=0&repo_id=382210636&response-content-disposition=attachment%3B%20filename%3Dcswin_tiny_224.pth&response-content-type=application%2Foctet-stream'
 
 model = dict(
     data_preprocessor=data_preprocessor,
 
     backbone=dict(
-        type='DCSwin',
-        encoder_channels=(128, 256, 512, 1024),
-        embed_dim=128,
-        depths=(2, 2, 18, 2),
-        num_heads=(4, 8, 16, 32),
-        frozen_stages=2,
-        init_cfg=dict(_delete_=True, type='Pretrained', checkpoint=checkpoint)
+        type='CSWin',
+        embed_dim=64,
+        depth=[1,2,21,1],
+        num_heads=[2,4,8,16],
+        split_size=[1,2,7,7],
+        drop_path_rate=0.3,
+        use_checkpoint=False,
+        init_cfg=dict(type='Pretrained', checkpoint=checkpoint)
     ),
-    
+
     decode_head=dict(
         type='myFCNHead',
-        in_channels=128,
-        channels=128,
         num_classes=10,
         loss_decode=[
             dict(type='CrossEntropyLoss', loss_name='loss_ce', use_sigmoid=False, loss_weight=0.3),
